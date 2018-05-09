7942313c5fff16f288f2d643b856e1c5128b99f9
Johnny Bell <hi@jonathanbell.com.au>
2018-05-08 22:34:48 -0700

    added some font awesome icons

--- src/app/components/Home.jsx
+++ src/app/components/Home.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
 import styled from 'styled-components';
+import FontAwesomeIcon from '@fortawesome/react-fontawesome';
+import faLinkedinIn from '@fortawesome/fontawesome-free-brands/faLinkedinIn';
+import faGithub from '@fortawesome/fontawesome-free-brands/faGithub';
+import faTwitter from '@fortawesome/fontawesome-free-brands/faTwitter';
+import faRss from '@fortawesome/fontawesome-free-solid/faRss';
 import backgroundImage from '../assets/images/bg.jpg';
 import Profile from '../assets/images/profile.jpg';
 
@@ -11,7 +16,7 @@ const HomePanel = styled.div`
 `;
 
 const Bio = styled.div`
-    max-width: 800px;
+    max-width: 700px;
     position: absolute;
     left: 0;
     right: 0;
@@ -19,6 +24,16 @@ const Bio = styled.div`
     transform: translateY(-50%);
     margin: 0 auto;
     text-align: center;
+    a svg {
+    transition: color 0.5s ease;
+        color:#ccc;
+        padding-right: 10px;
+        height:25px;
+        width:25px!important;
+    }
+    a:hover svg {
+        color:white;
+    }
 `;
 
 const ProfilePic = styled.img`
@@ -32,12 +47,12 @@ const ProfilePic = styled.img`
 const BioText = styled.p`
     color: #ccc;
     text-align: center;
-    font-size: 2.8rem;
+    font-size: 2.5rem;
     font-weight: 300;
     margin: 20px;
     padding: 0;
     strong {
-    color: white;
+      color: white;
     }
 `;
 
@@ -51,6 +66,10 @@ class Home extends React.Component {
                     <BioText>
                     Hi, I'm Johnny Bell, a 31 year old <strong>frontend engineer</strong> from <strong>Australia.</strong> Working with the awesome team at <strong>Doctor.com</strong> in <strong>Silicon Valley</strong>.
                     </BioText>
+                    <a href="https://www.linkedin.com/in/johnnyxbell/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faLinkedinIn} /></a>
+                    <a href="https://www.github.com/johnnyxbell/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faGithub} /></a>
+                    <a href="https://www.twitter.com/johnnyxbell/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faTwitter} /></a>
+                    <a href="https://blog.johnnybell.io" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faRss} /></a>
                 </Bio>
             </HomePanel>
         );
