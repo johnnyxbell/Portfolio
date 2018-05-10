f1ca7e2cfc3bdb8004b900d2972eae0ad489576f
Johnny Bell <johnnyb@corp.doctor.com>
2018-05-10 14:19:10 -0700

    added google analytics

--- src/app.jsx
+++ src/app.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'whatwg-fetch';
 import 'babel-polyfill';
+import ReactGA from 'react-ga';
+ReactGA.initialize('UA-73328361-1');
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 //components
 import AppIndex from './app/components/appIndex';
 
-ReactDOM.render(
-    <AppIndex/>,
-    document.getElementById('app')
-);
+ReactDOM.render(<AppIndex />, document.getElementById('app'));
